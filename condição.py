49e4d7f8f96b0790e502b34810e04bb2a7c3628a
Lara Candido Dantas <1352616179@ulife.com.br>
2026-08-20 11:38:46 -0300

    Fiz a alteração

--- condição.py
+++ condição.py
@@ -1,6 +1,6 @@
 nome = input("Digite seu nome: ")
 
-if nome = "Lara":
-    print("Olá, Vitória!")
-else
-    print("Olá, usuário!")
+if nome == "Lara":
+    print("Olá, Lara!")
+else:
+    print("Olá, Usuario!")
